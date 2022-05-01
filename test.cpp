6b85d311ff6979674b762c9c7df268c0a1084537
Violin <violinwang@mail.ustc.edu.cn>
2022-05-01 23:19:58 +0800

    Init commit

---
+++ test.cpp
@@ -0,0 +1,41 @@
+//
+// Created by violin on 2022/5/1.
+//
+
+#include <iostream>
+#include <fstream>
+#include "esminiLib.hpp"
+#include "esminiRMLib.hpp"
+
+using namespace std;
+
+int main() {
+    std::ifstream t("../resources/xosc/cut-in.xosc");
+    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    SE_InitWithString(str.c_str(), 0, 1, 0, 0);
+    int time = 10;
+    while (time--) {
+        SE_StepDT(0.01f);
+    }
+    SE_Close(); /* if you comment this line, it won't raise error */
+}
+
+
+/* if you comment this class, everything is OK */
+class Foo {
+public:
+    Foo();
+//    ~Foo();
+
+private:
+    int posHandle;
+};
+
+
+Foo::Foo() {
+    posHandle = RM_CreatePosition();
+}
+
+//Foo::~Foo() {
+//    RM_DeletePosition(posHandle);
+//}
